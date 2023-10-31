e382e9118ee005bcc200c4dc79f81bb79eeb4143
1215kkm <1215kkm@naver.com>
2023-10-31 09:23:43 +0900

    call js2

--- script/call.js
+++ script/call.js
@@ -1,1 +1,1 @@
-$('.teacher').html('성남 그린컴퓨터 강쌤반 <a href="tel:010-3321-7317"><i class="fa-solid fa-square-phone"></i> 질문 010-3321-7317</a>');
+$('.teacher').html('성남 그린컴퓨터 강쌤반 <a href="tel:010-3321-7317"><i class="fa-solid fa-square-phone"></i> 질문 010-3321-7317</a>');;
